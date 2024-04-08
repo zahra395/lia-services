eee2ea453b1ae294402d22159a0f59544237f35c
zahra395 <ahmadi.zahra395@gmail.com>
2024-04-08 19:39:37 +0430

    Add get, delete and update endpoints for product module

--- src/routes/products.py
+++ src/routes/products.py
@@ -1,5 +1,7 @@
-from beanie import WriteRules
-from fastapi import APIRouter
+from beanie import WriteRules, PydanticObjectId
+
+from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter, Depends, Path, HTTPException, Body
 
 from src.schemas.products import *
 from src.models.models import Product
@@ -17,3 +19,74 @@ async def create_product(args: CreateProductIn):
                        timeGenerated=datetime.now(),
                        message="Product added successfully"
                        )
+
+
+@product_route.get("/", tags=["Products"], response_model=AllResponseOut)
+async def get_all_products(args: GetAllProductsIn = Depends(GetAllProductsIn)):
+    records = await Product.find(limit=args.page_size, skip=(args.page_size * (args.page_number - 1))).to_list()
+    serialized_records = jsonable_encoder(records)
+
+    return AllResponseOut(result=serialized_records,
+                          timeGenerated=datetime.now(),
+                          message="Get Products Successfully"
+                          )
+
+
+@product_route.get("/{product_id}/", tags=["Products"], response_model=ResponseOut)
+async def get_product(product_id: PydanticObjectId = Path(...)):
+    product = await Product.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return ResponseOut(
+        result=product.dict(by_alias=True),
+        timeGenerated=datetime.now(),
+        message="Product retrieved successfully"
+    )
+
+
+@product_route.get("/{product_id}/", tags=["Products"], response_model=ResponseOut)
+async def update_product(product_id: PydanticObjectId = Path(...)):
+    product = await Product.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return ResponseOut(
+        result=product.dict(by_alias=True),
+        timeGenerated=datetime.now(),
+        message="Product retrieved successfully"
+    )
+
+
+@product_route.delete("/{product_id}/", tags=["Products"], response_model=ResponseOut)
+async def delete_product(product_id: PydanticObjectId = Path(...)):
+    product = await Product.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    await product.delete()
+
+    return ResponseOut(
+        result={},
+        timeGenerated=datetime.now(),
+        message="Product deleted successfully"
+    )
+
+
+@product_route.put("/{product_id}/", tags=["Products"], response_model=ResponseOut)
+async def update_product(product_id: PydanticObjectId = Path(...),
+                         args: UpdateProductIn = Body(...)
+                         ):
+    product = await Product.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    update_data = args.dict()
+    update_query = {"$set": {field: value for field, value in update_data.items() if value is not None}}
+    await product.update(update_query)
+
+    return ResponseOut(
+        result=product.dict(by_alias=True),
+        timeGenerated=datetime.now(),
+        message="Product updated successfully"
+    )
--- src/schemas/products.py
+++ src/schemas/products.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
+from fastapi import Query
 from pydantic import BaseModel, validator
-from typing import Dict, Any
+from typing import Dict, Optional, Any, List
 
 
 class CommonResponse(BaseModel):
@@ -17,6 +18,27 @@ class CreateProductIn(BaseModel):
     name: str
     price: float
     description: str
+
+    @validator('price')
+    def price_must_be_positive(cls, value):
+        if value < 0:
+            raise ValueError('Price must be greater than or equal to zero.')
+        return value
+
+
+class AllResponseOut(CommonResponse):
+    result: List[Dict[str, Any]]
+
+
+class GetAllProductsIn(BaseModel):
+    page_number: Optional[int] = Query(1, ge=1)
+    page_size: Optional[int] = Query(10, ge=1)
+
+
+class UpdateProductIn(BaseModel):
+    name: str = None
+    price: float = None
+    description: str = None
 
     @validator('price')
     def price_must_be_positive(cls, value):
